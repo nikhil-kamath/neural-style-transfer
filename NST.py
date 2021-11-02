146b069fd2d9c3b8aa66fbdc0ef8e6a140015d51
Nikhil Kamath <58089299+goober3@users.noreply.github.com>
2021-11-02 16:05:09 -0400

    fixed images

--- NST.py
+++ NST.py
@@ -23,8 +23,43 @@ def tensor_to_image(tensor):
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
 
-content_path = tf.keras.utils.get_file('me_walking.JPG')
-style_path = tf.keras.utils.get_file('blue_abstract_lines.jpg')
+
+content_path = tf.keras.utils.get_file(
+    'me_walking.JPG', 'https://raw.githubusercontent.com/nikhil-kamath/neural-style-transfer/main/me_walking.JPG')
+style_path = tf.keras.utils.get_file(
+    'blue_abstract_lines.jpg', 'https://raw.githubusercontent.com/nikhil-kamath/neural-style-transfer/main/blue_abstract_lines.jpg')
 
 def load_img(path_to_image):
-    pass
+    max_dim = 512
+    img = tf.io.read_file(path_to_image)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+    
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+plt.subplot(1, 2, 1)
+imshow(content_image, 'Content Image')
+
+plt.subplot(1, 2, 2)
+imshow(style_image, 'Style Image')
+
+
