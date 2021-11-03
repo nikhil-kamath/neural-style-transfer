46937a46279195054cc79c5b507f1d48217180ba
Nikhil Kamath <58089299+goober3@users.noreply.github.com>
2021-11-02 20:35:10 -0400

    working version

--- NST.py
+++ NST.py
@@ -1,4 +1,6 @@
 import os
+from re import L
+from numpy.core.numeric import extend_all
 import tensorflow as tf
 
 os.environ["TFHUB_MODEL_LOAD_FORMAT"] = 'COMPRESSED'
@@ -24,11 +26,11 @@ def tensor_to_image(tensor):
     return PIL.Image.fromarray(tensor)
 
 
-style_path = tf.keras.utils.get_file(
-    'blue_abstract_line.jpg', 'https://raw.githubusercontent.com/nikhil-kamath/neural-style-transfer/main/blue_abstract_lines.jpg')
-
 content_path = tf.keras.utils.get_file(
-    'me_walking.png', 'https://github.com/nikhil-kamath/neural-style-transfer/blob/main/me_walking.png')
+    'me_walking.jpeg', 'https://raw.githubusercontent.com/nikhil-kamath/neural-style-transfer/main/me_walking.jpeg')
+style_path = tf.keras.utils.get_file(
+    'warms.png', 'https://designshack.net/wp-content/uploads/warm-colors-in-design.png')
+
 
 
 
@@ -64,6 +66,102 @@ imshow(content_image, 'Content Image')
 
 plt.subplot(1, 2, 2)
 imshow(style_image, 'Style Image')
+
+
+content_layers = ['block5_conv2']
+
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+from StyleContentModel import StyleContentModel
+
+extractor = StyleContentModel(style_layers, content_layers)
+
+results = extractor(tf.constant(content_image))
+
+print('Styles:')
+for name, output in sorted(results['style'].items()):
+    print("  ", name)
+    print("    shape: ", output.numpy().shape)
+    print("    min: ", output.numpy().min())
+    print("    max: ", output.numpy().max())
+    print("    mean: ", output.numpy().mean())
+    print()
+
+print("Contents:")
+for name, output in sorted(results['content'].items()):
+    print("  ", name)
+    print("    shape: ", output.numpy().shape)
+    print("    min: ", output.numpy().min())
+    print("    max: ", output.numpy().max())
+    print("    mean: ", output.numpy().mean())
+
+style_targets = extractor(style_image)['style']
+content_targets = extractor(content_image)['content']
+
+image = tf.Variable(content_image)
+
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0., clip_value_max=1.)
+
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+style_weight = 1e-2
+content_weight = 1e4
+
+def style_content_loss(outputs):
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) for name in content_outputs.keys()])
+    content_loss *= content_weight / num_content_layers
+    return style_loss + content_loss
+
+@tf.function()
+def train_step(image):
+    with tf.GradientTape() as tape:
+        outputs = extractor(image)
+        loss = style_content_loss(outputs)
+    
+    grad = tape.gradient(loss, image)
+    opt.apply_gradients([(grad, image)])
+    image.assign(clip_0_1(image))
+
+
+test_filepath = 'test.jpg'
+train_step(image)
+train_step(image)
+train_step(image)
+tensor_to_image(image).save(test_filepath)
+
+start = time.time()
+
+epochs = 10
+steps_per_epoch = 100
+
+step = 0
+for _ in range(epochs):
+    for _ in range(steps_per_epoch):
+        step += 1
+        train_step(image)
+        print(".", end='', flush=True)
+    display.clear_output(wait=True)
+    display.display(tensor_to_image(image))
+    print("Train step: {}".format(step))
+
+end = time.time()
+print("Total time: {:.1f}".format(end-start))
+
+final_filepath = 'final.jpg'
+tensor_to_image(image).save(final_filepath)
 
 
 plt.show()
